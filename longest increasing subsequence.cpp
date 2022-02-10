97d1548c020a82a43cf74460e04e478369e67334
pratyush15 <60066959+pratyush15@users.noreply.github.com>
2022-02-10 14:50:50 +0530

    Add files via upload

---
+++ longest increasing subsequence.cpp
@@ -0,0 +1,30 @@
+#include <iostream> 
+#include <bits/stdc++.h>
+using namespace std;
+int lis(int arr[], int n)
+{
+	int lis[n];
+
+	lis[0] = 1;
+	for (int i = 1; i < n; i++) {
+		lis[i] = 1;
+		for (int j = 0; j < i; j++)
+			if (arr[i] > arr[j] && lis[i] < lis[j] + 1)
+				lis[i] = lis[j] + 1;
+	}
+	return *max_element(lis, lis + n);
+}
+int main()
+{
+	int arr[1000]; 
+	int n; 
+	cin>>n; 
+	for(int i=0;i<n;i++) 
+	{
+		cin>>arr[i]; 
+	}
+	cout<<"Length of lis is\n"<<lis(arr, n);
+
+	return 0;
+}
+
