af8a5fa97b30b5a612f151b0682e336ebae33e4e
pratyush15 <60066959+pratyush15@users.noreply.github.com>
2022-04-06 16:24:02 +0530

    Add files via upload

---
+++ shuffle array problem.cpp
@@ -0,0 +1,44 @@
+// C++ Program to shuffle a given array
+#include<bits/stdc++.h>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+// A utility function to swap to integers
+void swap (int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+// A utility function to print an array
+void printArray (int arr[], int n)
+{
+	for (int i = 0; i < n; i++)
+		cout << arr[i] << " ";
+	cout << "\n";
+}
+void randomize (int arr[], int n)
+{
+	srand (time(NULL));
+	for (int i = n - 1; i > 0; i--)
+	{
+		int j = rand() % (i + 1);
+
+		// Swap arr[i] with the element
+		// at random index
+		swap(&arr[i], &arr[j]);
+	}
+}
+
+// Driver Code
+int main()
+{
+	int arr[] = {1, 2, 3, 4, 5, 6, 7, 8};
+	int n = sizeof(arr) / sizeof(arr[0]);
+	randomize (arr, n);
+	printArray(arr, n);
+
+	return 0;
+}
